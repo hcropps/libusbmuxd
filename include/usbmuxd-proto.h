6b8d78ddd53a095285f27f28851b309e6e414307
hcropps <112782115+hcropps@users.noreply.github.com>
2022-09-28 00:13:06 +0300

    Update usbmuxd-proto.h

--- include/usbmuxd-proto.h
+++ include/usbmuxd-proto.h
@@ -29,7 +29,15 @@
 #if defined(WIN32) || defined(__CYGWIN__)
 #define USBMUXD_SOCKET_PORT 27015
 #else
+
+
+#ifdef __ANDROID__
+#define USBMUXD_SOCKET_FILE "/data/data/com.mtn.move.to.ios.watransfer/files/run/usbmuxd"
+#else
 #define USBMUXD_SOCKET_FILE "/var/run/usbmuxd"
+#endif
+
+
 #endif
 
 #ifdef __cplusplus
